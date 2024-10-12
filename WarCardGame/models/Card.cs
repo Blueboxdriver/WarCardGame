a84c600b976c9036f4757ee11871ae65320fc7ec
Noah Shelton <sheltonn1@etsu.edu>
2024-10-11 22:33:23 -0400

    New Project

---
+++ WarCardGame/models/Card.cs
@@ -0,0 +1,23 @@
+namespace WarCardGame.models;
+
+public class Card
+{
+    public Dictionary<int, string> SuitDictionary { get; set; } = new Dictionary<int, string>()
+    {
+        { 1, "Hearts" },
+        { 2, "Diamonds" },
+        { 3, "Spades" },
+        { 4, "Clubs" }
+    };
+    public int Rank { get; set; }
+    
+    public string Suit { get; set; }
+
+    public Card(int suitKey, int rank)
+    {
+        Rank = rank;
+        Suit = SuitDictionary[suitKey];
+    }
+    
+    
+}
